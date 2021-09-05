139cb791ea598bf9d01b1daf92850ee62732014b
Nathaniel Silverman <54076746+nathanielsilverman@users.noreply.github.com>
2021-09-05 17:29:24 -0400

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-	mongoURI: 'mongodb+srv://nathaniel:Git-g0d99@cluster0.bhsx0.mongodb.net/myShoppingList?retryWrites=true&w=majority'
+	mongoURI: 'mongodb+srv://nathaniel:react-App_123@cluster0.bhsx0.mongodb.net/myShoppingList?retryWrites=true&w=majority'
 };
